b66607ce6255b18ebc52518edf37d225eee4260c
Alok Kumar <109469008+alok7642@users.noreply.github.com>
2024-02-29 23:34:40 +0000

    conversion of datatypes

---
+++ 01_basics/3_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score = '33abc'
+// let score = null
+// let score = true
+// let score = 'alok'
+
+// console.log(typeof score);
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+                //    ----
+// let isLoggedIn = '';
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// 1 =>true ; 0 => false
+// "" => false
+// 'alok' => true
+
+                    //    -----
+let number =66;
+let stringNumber = String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
