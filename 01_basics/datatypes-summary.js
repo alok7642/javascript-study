54e49d5c83321fd06702cdd68d078be1a9065b3d
Alok Kumar <109469008+alok7642@users.noreply.github.com>
2024-03-01 17:56:44 +0000

    study diff datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// primitive 
+// 7 types :String,Number,null,undefined,Boolean,Symbol,BigInt
+
+const score=100
+const scoreVal = 100.3 //there is not need to define the type of datatypes so js is dynamically typed language , but typeScript is statically typed we have to define first 
+let outsideTemp = null;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+console.log(id === anotherId);
+
+const bigNum = 333333333333333333333333333333333923456789923456789n;
+console.log(bigNum);
+
+// Reference (Non primitive)
+//Array , Object , Function
+
+const heroes = ['shaktiman','naagraj' , 'doga'];
+let myObj = {
+    name:'alok',
+    age:22,
+ }
+ const myFunction = function () {
+    console.log('hello world')
+ }
+
+ console.log(typeof bigNum);
+ console.log(typeof outsideTemp);
+ console.log(typeof scoreVal);
+ console.log(typeof myFunction);
+ console.log(typeof anotherId);
+
