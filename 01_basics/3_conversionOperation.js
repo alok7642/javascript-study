5aa0542a50971fc7d968e8b32bac33a766eb4217
Alok Kumar <109469008+alok7642@users.noreply.github.com>
2024-03-01 00:14:36 +0000

    conversion & operation

--- 01_basics/3_conversionOperation.js
+++ 01_basics/3_conversionOperation.js
@@ -21,7 +21,31 @@
 // 'alok' => true
 
                     //    -----
-let number =66;
-let stringNumber = String(number)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let number =66;
+// let stringNumber = String(number)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ****************operations***********
+// let str1='alok'
+// let str2=' kumar';
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log('1' + 2);
+// console.log('2' + 1);
+// console.log(1 + 2 + '3');
+// console.log('2' + 4 + 5);
+
+// console.log(-true+4);
+// console.log(+'');
+
+let x=3
+const y=x++ //first assign the value of x to the y then increment is done 
+console.log(`x:${x}`)
+console.log(`y:${y}`);
+
+let a=3
+const b=++a // first increse the  value of a then assign it to the b  
+console.log(`a:${a}`)
+console.log(`b:${b}`);
