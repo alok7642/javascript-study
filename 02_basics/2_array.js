b51c456005bda0a8ac2d71a695a61f97480cded4
Alok Kumar <109469008+alok7642@users.noreply.github.com>
2024-03-02 20:45:44 +0000

    array second

---
+++ 02_basics/2_array.js
@@ -0,0 +1,10 @@
+const marvelHeroes = ['thor','ironman',"spiderman"]
+const dcHeroes = ['superman' , 'flash' , 'batman']
+
+marvelHeroes.push(dcHeroes)
+console.log(marvelHeroes[0]);
+
+console.log(marvelHeroes.toString());
+console.log(marvelHeroes.length);
+
+console.log(marvelHeroes[3.2]);
