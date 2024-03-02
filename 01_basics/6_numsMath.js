686ad6947dd8a96bc2b33da870a6742a83e8cc5f
Alok Kumar <109469008+alok7642@users.noreply.github.com>
2024-03-02 07:00:38 +0000

    number and math

---
+++ 01_basics/6_numsMath.js
@@ -0,0 +1,39 @@
+// const score = 30.6958
+// console.log(score);
+// const balance = new Number(score)
+// console.log(balance);
+
+// let a = console.log(balance.toString());
+// console.log(typeof a);
+
+// console.log(score.toFixed(3));
+// console.log(score.toPrecision(4));
+
+// const hun = 1000000;
+// console.log(hun.toLocaleString()); //it will put comma ac to international 
+// console.log(hun.toLocaleString('en-IN')); //indian number system
+
+// console.log(hun.MAX_VALUE());
+
+
+// +++++++++++++   math ++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4)); //4
+// console.log(Math.round(4.4)); 
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9))
+
+
+// console.log(Math.min(4,5,6,7));
+// console.log(Math.max(4,5,6,7));
+
+
+console.log(Math.random())
+console.log(Math.random() * 10)
+console.log(Math.floor(Math.random() * 10) + 1)
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
