2649ec95223a100efdad4601e4d934b9d1b83898
Alok Kumar <109469008+alok7642@users.noreply.github.com>
2024-03-02 07:01:02 +0000

    date function

---
+++ 01_basics/7_date.js
@@ -0,0 +1,22 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023,0,23)
+
+// let myCreatedDate = new Date(2023,0,23 , 5 , 3)
+
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("1-14-2023") //mm-dd-yyyy
+
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
