51431acfd2836b9b124c529b8504ab6533c1136d
Alok Kumar <109469008+alok7642@users.noreply.github.com>
2024-03-07 19:51:14 +0000

    function defining

--- 03_basics/1_function.js
+++ 03_basics/1_function.js
@@ -14,7 +14,7 @@ sayMyname();
 
 
 
-function addTwoNumber(num1 , num2){ //here num1 & num2 is parameters 
+function addTwoNumber(num1 , num2){ 
 // let result = num1+num2;
 // return result; 
 return num1+num2; //we can also do like this
@@ -23,14 +23,14 @@ const result = addTwoNumber(3,5)
 console.log('result :',result)
 
 
-// function loginUserMessage(username = 'sam'){
-//     if(username === undefined){
-//         console.log('pls enter a username')
-//         return;
-//     }
-//     return `${username} just logged in`
-// }
-// console.log(loginUserMessage('Alok'));
+function loginUserMessage(username){
+    if(username === undefined){
+        console.log('pls enter a username')
+        return;
+    }
+    return `${username} just logged in`
+}
+console.log(loginUserMessage('Alok'));
 // console.log(loginUserMessage())
 
 
@@ -43,3 +43,35 @@ function loginUserMessage(username ='sam' ){ //if we want to give a default valu
 }
 // console.log(loginUserMessage('Alok')); //if we pass value then it will be updated else default value will generate
 console.log(loginUserMessage())
+
+
+function calCartPrice(val1,val2,...num1){
+    return num1;
+}
+console.log(calCartPrice(100,200,300,400));
+
+
+const user = {
+      username:'Alok',
+      prices:800
+}
+function handelObject(anyobject){
+    console.log(`username is ${anyobject.username} and prices is ${anyobject.prices}`)
+}
+// handelObject(user)
+handelObject({ //we can also do like this
+    username:'Aman',
+    prices:400
+})
+
+
+
+const myNewArray = [200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([900,800,700,600]))
+
