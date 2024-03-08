877cc7d4c2863b4e699dbcbb8f3d2a973810b774
Alok Kumar <109469008+alok7642@users.noreply.github.com>
2024-03-08 12:40:20 +0000

    iife defining

---
+++ 03_basics/4_iife.js
@@ -0,0 +1,11 @@
+// IMMEDIATELY INVOKED Function EXPRESSION
+
+(function chai(){  //named iife
+    console.log(`we r studying`);
+})();   // when writing two iife ; is necessary
+
+
+
+((name)=>{ //nameless iife
+    console.log(`I am coding ${name}`);
+})('javascript')
