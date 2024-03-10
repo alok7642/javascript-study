5c70199320dff046ec1d979ddf4b476ed2a69f21
Alok Kumar <109469008+alok7642@users.noreply.github.com>
2024-03-10 18:46:36 +0000

    truty vlaue

---
+++ 04_controlFlow/truty.js
@@ -0,0 +1,50 @@
+const userEmail =[ ]
+
+if(userEmail){
+    console.log('got user email');
+}
+else{
+    console.log('not registerd ');
+}
+
+//flasy value 
+// false , 0 , -0 , BigInt 0n , null , undefined , NaN
+
+//truthy vlaue
+// '0' , 'false' , [] , {} , function(){}
+
+// if (userEmail.length === 0){
+//     console.log('Array is empty');
+// }
+
+// const emptyObj = {} 
+
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("Object  empty");
+// }
+
+
+// // Nullish Coalescing Operator (??) : null undefined
+
+// let val1;
+// // val1 = 5 ?? 10
+// // val1 = null ?? 10
+// val1 = undefined ?? 11
+// val1 = null ?? 15 ?? 20
+
+// console.log(val1);
+
+
+
+// // Terniary Operator 
+
+// let a = 101
+// let b = 20 
+
+
+// console.log(a < b ? b +  " is greater" : a + " is smaller ")
+
+// let temp = 10 
+
+// temp > 30 ? console.log("temp is greater " ) : console.log('temp is samller');
+
