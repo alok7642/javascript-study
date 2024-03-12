b7687f8f3f4d66a34324a19866b5bc97d4abffc5
Alok Kumar <109469008+alok7642@users.noreply.github.com>
2024-03-12 07:39:31 +0000

    while loop

---
+++ 05_iteration/while.js
@@ -0,0 +1,23 @@
+// while loop 
+
+// let index = 0 ;
+// while (index <= 10) {
+//     console.log(`The Even Number are : ${index }`);
+//     index = index + 2; 
+// }
+
+let superhero= ['batman ' , 'spiderman' , 'ironman' , 'shaktiman ']
+// for(let i =0 ; i < superhero.length; i++){
+//     console.log(superhero[i]);
+// }
+// let i = 0 ;
+// while( i < superhero.length){
+//     console.log(superhero[i]);
+//     i++; 
+// }
+
+let i =1;
+do {
+    console.log(i);
+    i++;
+} while (i<=10);
